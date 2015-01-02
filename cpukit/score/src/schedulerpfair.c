b85f657c8d8e0a67785562332fcaa924017df9b9
Hesham ALMatary <heshamelmatary@gmail.com>
2015-01-02 14:36:54 +0000

    Add stub files for pfair scheduler

---
+++ cpukit/score/src/schedulerpfair.c
@@ -0,0 +1,21 @@
+/**
+ * @file
+ *
+ * @brief Scheduler Pfair Initialize and Support
+ * @ingroup ScoreScheduler
+ */
+
+/*
+ *  Copyright (C) 2015 Hesham ALMatary.
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.org/license/LICENSE.
+ */
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <rtems/score/schedulerpfairimpl.h>
+
