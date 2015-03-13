3f10adddbc67c478567742aed2c12bf8039d6607
Hesham ALMatary <heshamelmatary@gmail.com>
2015-03-13 17:02:36 +0000

    Add support for tm27

--- c/src/lib/libbsp/epiphany/parallella/include/tm27.h
+++ c/src/lib/libbsp/epiphany/parallella/include/tm27.h
@@ -5,16 +5,17 @@
 #ifndef __tm27_h
 #define __tm27_h
 
+#include <rtems/score/epiphany-utility.h>
 /**
  * @name Interrupt mechanisms for Time Test 27 
  * @{
  */
 
-#define MUST_WAIT_FOR_INTERRUPT 0
+#define MUST_WAIT_FOR_INTERRUPT 1
 
-#define Install_tm27_vector( handler ) /* empty */
+#define Install_tm27_vector( handler ) *((uint32_t *) 0x3C) = handler
 
-#define Cause_tm27_intr() /* empty */
+#define Cause_tm27_intr() write_epiphany_reg((0x808 << 20 | EPIPHANY_PER_CORE_REG_ILATST), 1 << SMP_MESSAGE)
 
 #define Clear_tm27_intr() /* empty */
 
